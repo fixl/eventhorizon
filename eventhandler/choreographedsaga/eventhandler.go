6f63bea6c2c3d0fc3de593ac464ff98e94d12f3a
Stefan Fuchs <me@fixl.info>
2021-03-06 15:43:31 +1100

    Add chorographed saga

---
+++ eventhandler/choreographedsaga/eventhandler.go
@@ -0,0 +1,123 @@
+// Copyright (c) 2021 - The Event Horizon authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package choreographedsaga
+
+import (
+	"context"
+	"fmt"
+	"github.com/google/uuid"
+	eh "github.com/looplab/eventhorizon"
+)
+
+// EventHandler is a CQRS saga handler to run a Saga implementation with a fully loaded aggregate
+type EventHandler struct {
+	saga           Saga
+	aggregateStore eh.AggregateStore
+	commandHandler eh.CommandHandler
+}
+
+// Saga is an interface for a CQRS saga that listens to events and generates commands
+// A fully loaded aggregate is passed to the saga in order make decisions about
+type Saga interface {
+	// SagaType returns the type of the saga.
+	SagaType() Type
+
+	// Aggregate extracts the aggregate type and ID for which aggregate to load.
+	// Because a saga can be choreographed across many aggregates, this varies from event to event.
+	Aggregate(event eh.Event) (eh.AggregateType, uuid.UUID, error)
+
+	// RunSaga handles an event in saga that can returns commands.
+	// A fully loaded aggregate is passed to the saga to allow making
+	// business decisions based on current state.
+	RunSaga(context.Context, eh.Aggregate, eh.Event, eh.CommandHandler) error
+}
+
+// Type is the type of a sage, used as its unique identifier
+type Type string
+
+// String returns the string representation of a saga type.
+func (t Type) String() string {
+	return string(t)
+}
+
+// Error is an error in the saga, with the namespace.
+type Error struct {
+	// Err is the error that happened when projecting the event.
+	Err error
+	// Saga is the saga where the error happened.
+	Saga string
+	// Namespace is the namespace for the error.
+	Namespace string
+}
+
+// Error implements the Error method of the errors.Error interface.
+func (e Error) Error() string {
+	return fmt.Sprintf("%s: %s (%s)", e.Saga, e.Err, e.Namespace)
+}
+
+// Unwrap implements the errors.Unwrap method.
+func (e Error) Unwrap() error {
+	return e.Err
+}
+
+// Cause implements the github.com/pkg/errors Unwrap method.
+func (e Error) Cause() error {
+	return e.Unwrap()
+}
+
+// NewEventHandler creates a new EventHandler
+func NewEventHandler(saga Saga, aggregateStore eh.AggregateStore, commandHandler eh.CommandHandler) *EventHandler {
+	return &EventHandler{
+		saga:           saga,
+		aggregateStore: aggregateStore,
+		commandHandler: commandHandler,
+	}
+}
+
+// HandlerType implements the HandlerType method of the eventhorizon.EventHandler interface.
+func (h *EventHandler) HandlerType() eh.EventHandlerType {
+	return eh.EventHandlerType("aggregate_saga_" + h.saga.SagaType())
+}
+
+// HandleEvent implements the HandleEvent method of the eventhorizon.EventHandler interface.
+func (h *EventHandler) HandleEvent(ctx context.Context, event eh.Event) error {
+	aggregateType, aggregateID, err := h.saga.Aggregate(event)
+	if err != nil {
+		return Error{
+			Err:       err,
+			Saga:      h.saga.SagaType().String(),
+			Namespace: eh.NamespaceFromContext(ctx),
+		}
+	}
+
+	aggregate, err := h.aggregateStore.Load(ctx, aggregateType, aggregateID)
+	if err != nil {
+		return Error{
+			Err:       err,
+			Saga:      h.saga.SagaType().String(),
+			Namespace: eh.NamespaceFromContext(ctx),
+		}
+	}
+
+	if err := h.saga.RunSaga(ctx, aggregate, event, h.commandHandler); err != nil {
+		return Error{
+			Err:       err,
+			Saga:      h.saga.SagaType().String(),
+			Namespace: eh.NamespaceFromContext(ctx),
+		}
+	}
+
+	return nil
+}
---
+++ eventhandler/choreographedsaga/eventhandler_test.go
@@ -0,0 +1,171 @@
+// Copyright (c) 2021 - The Event Horizon authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package choreographedsaga
+
+import (
+	"context"
+	"errors"
+	"github.com/google/uuid"
+	eh "github.com/looplab/eventhorizon"
+	"github.com/looplab/eventhorizon/mocks"
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestEventHandler(t *testing.T) {
+	commandHandler := &mocks.CommandHandler{
+		Commands: []eh.Command{},
+	}
+
+	a := mocks.NewAggregate(uuid.New())
+	aggregateStore := &mocks.AggregateStore{
+		Aggregates: map[uuid.UUID]eh.Aggregate{
+			a.EntityID(): a,
+		},
+	}
+	saga := &TestSaga{}
+	handler := NewEventHandler(saga, aggregateStore, commandHandler)
+
+	ctx := context.Background()
+
+	eventData := &TestEventData{
+		TestAggregateID: a.EntityID(),
+	}
+	timestamp := time.Date(2021, time.March, 6, 14, 0, 0, 0, time.UTC)
+	event := eh.NewEvent(mocks.EventType, eventData, timestamp,
+		eh.ForAggregate(mocks.AggregateType, a.EntityID(), 1))
+	saga.commands = []eh.Command{&mocks.Command{ID: event.AggregateID(), Content: "content"}}
+
+	if err := handler.HandleEvent(ctx, event); err != nil {
+		t.Error("there should be no error:", err)
+	}
+	if saga.event != event {
+		t.Error("the handled event should be correct: ", saga.event)
+	}
+	if !reflect.DeepEqual(commandHandler.Commands, saga.commands) {
+		t.Error("the produced commands should be correct: ", commandHandler.Commands)
+	}
+	if saga.aggregate != a {
+		t.Error("the loaded aggregate should be correct: ", saga.aggregate)
+	}
+}
+
+func TestEventProcessFail(t *testing.T) {
+	commandHandler := &mocks.CommandHandler{
+		Commands: []eh.Command{},
+	}
+
+	a := mocks.NewAggregate(uuid.New())
+	aggregateStore := &mocks.AggregateStore{
+		Aggregates: map[uuid.UUID]eh.Aggregate{
+			a.EntityID(): a,
+		},
+	}
+	saga := &TestSaga{}
+	handler := NewEventHandler(saga, aggregateStore, commandHandler)
+
+	ctx := context.Background()
+
+	eventData := &mocks.EventData{
+		Content: "content",
+	}
+	timestamp := time.Date(2021, time.March, 6, 14, 0, 0, 0, time.UTC)
+	event := eh.NewEvent(mocks.EventType, eventData, timestamp,
+		eh.ForAggregate(mocks.AggregateType, a.EntityID(), 1))
+
+	expectedError := Error{
+		Err:       errors.New("cannot process event"),
+		Saga:      saga.SagaType().String(),
+		Namespace: eh.NamespaceFromContext(ctx),
+	}
+	err := handler.HandleEvent(ctx, event)
+
+	if !errors.Is(err, expectedError) {
+		t.Error("there should be an error", err)
+	}
+}
+
+func TestAggregateLoadFail(t *testing.T) {
+	commandHandler := &mocks.CommandHandler{
+		Commands: []eh.Command{},
+	}
+
+	a := mocks.NewAggregate(uuid.New())
+	aggregateStore := &mocks.AggregateStore{
+		Err: errors.New("aggregate does not exist"),
+	}
+	saga := &TestSaga{}
+	handler := NewEventHandler(saga, aggregateStore, commandHandler)
+
+	ctx := context.Background()
+
+	eventData := &TestEventData{
+		TestAggregateID: a.EntityID(),
+	}
+	timestamp := time.Date(2021, time.March, 6, 14, 0, 0, 0, time.UTC)
+	event := eh.NewEvent(mocks.EventType, eventData, timestamp,
+		eh.ForAggregate(mocks.AggregateType, a.EntityID(), 1))
+
+	expectedError := Error{
+		Err:       aggregateStore.Err,
+		Saga:      saga.SagaType().String(),
+		Namespace: eh.NamespaceFromContext(ctx),
+	}
+	err := handler.HandleEvent(ctx, event)
+
+	if !errors.Is(err, expectedError) {
+		t.Error("there should be an error", err)
+	}
+}
+
+type TestEventData struct {
+	TestAggregateID uuid.UUID
+	Err             error
+}
+
+const (
+	TestSagaType Type = "TestSaga"
+)
+
+type TestSaga struct {
+	event     eh.Event
+	aggregate eh.Aggregate
+	context   context.Context
+	commands  []eh.Command
+}
+
+func (s *TestSaga) SagaType() Type {
+	return TestSagaType
+}
+
+func (s *TestSaga) Aggregate(event eh.Event) (eh.AggregateType, uuid.UUID, error) {
+	if data, ok := event.Data().(*TestEventData); ok {
+		return mocks.AggregateType, data.TestAggregateID, nil
+	}
+
+	return "", uuid.Nil, errors.New("cannot process event")
+}
+
+func (s *TestSaga) RunSaga(ctx context.Context, aggregate eh.Aggregate, event eh.Event, h eh.CommandHandler) error {
+	s.context = ctx
+	s.aggregate = aggregate
+	s.event = event
+	for _, cmd := range s.commands {
+		return h.HandleCommand(ctx, cmd)
+	}
+
+	return nil
+}
